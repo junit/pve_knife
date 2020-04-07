c9fbe50d9372276444bce257237a2d69e4f15d80
yiwen.lu <yiwen.lu@guruhk.com>
2020-04-07 14:37:30 +0800

    update optimize for V6

--- func/Download.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DownloadVirtIOISO() {
-    aria2c -x -c -o /var/lib/vz/template/iso/virtio-win.iso https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso
-}
--- func/Repo.sh
+++ func/Repo.sh
@@ -2,30 +2,30 @@
 ReplaceEnterpriseSource() {
     if [ -f "/etc/apt/sources.list.d/pve-enterprise.list"  ]; then
     mv /etc/apt/sources.list.d/pve-enterprise.list /etc/apt/sources.list.d/pve-enterprise.list.bak
-    echo -e 'deb http://download.proxmox.com/debian/pve stretch pve-no-subscription\n' > /etc/apt/sources.list.d/pve-community.list
+    echo -e 'deb https://mirrors.ustc.edu.cn/proxmox/debian/pve buster pve-no-subscription\n' > /etc/apt/sources.list.d/pve-community.list
     fi
     echo "Source replacement already complete"
 }
 
 ReplaceDebianUpdateRepo() {
     cat > /etc/apt/sources.list <<EOF
-deb http://mirrors.aliyun.com/debian/ stretch main non-free contrib
-deb-src http://mirrors.aliyun.com/debian/ stretch main non-free contrib
-deb http://mirrors.aliyun.com/debian-security stretch/updates main
-deb-src http://mirrors.aliyun.com/debian-security stretch/updates main
-deb http://mirrors.aliyun.com/debian/ stretch-updates main non-free contrib
-deb-src http://mirrors.aliyun.com/debian/ stretch-updates main non-free contrib
-deb http://mirrors.aliyun.com/debian/ stretch-backports main non-free contrib
-deb-src http://mirrors.aliyun.com/debian/ stretch-backports main non-free contrib
+deb https://mirrors.ustc.edu.cn/debian/ buster main contrib non-free
+deb https://mirrors.ustc.edu.cn/debian/ buster-updates main contrib non-free
+deb https://mirrors.ustc.edu.cn/debian/ buster-backports main contrib non-free
+deb https://mirrors.ustc.edu.cn/debian-security/ buster/updates main contrib non-free
+deb-src https://mirrors.ustc.edu.cn/debian/ buster main contrib non-free
+deb-src https://mirrors.ustc.edu.cn/debian/ buster-updates main contrib non-free
+deb-src https://mirrors.ustc.edu.cn/debian/ buster-backports main contrib non-free
+deb-src https://mirrors.ustc.edu.cn/debian-security/ buster/updates main contrib non-free
 EOF
 }
 
 AddReserveProxy() {
 # Add For Proxmox Update
-if [ `grep -c "89.31.125.19 download.proxmox.com" /etc/hosts` != '0' ]; then
-	echo 'Done'
-else
-    echo "89.31.125.19 download.proxmox.com" >> /etc/hosts
-fi
+#if [ `grep -c "89.31.125.19 download.proxmox.com" /etc/hosts` != '0' ]; then
+#	echo 'Done'
+#else
+#    echo "89.31.125.19 download.proxmox.com" >> /etc/hosts
+#fi
 }
 
